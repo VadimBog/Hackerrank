434507e3e982f5b1d9cfa85918a5ab3318afbc97
Vadim Bogomazov <vadimbogomazov@yahoo.com>
2024-03-29 12:44:10 -0500

    Python Integers Come In All Sizes

---
+++ Py Integers Come In All Sizes.py
@@ -0,0 +1,18 @@
+# Integers in Python can be as big as the bytes in your machine's memory. 
+# There is no limit in size as there is:  2^31 - 1(c++ int) or  2^63 - 1(C++ long long int).
+
+# As we know, the result of a^b grows really fast with increasing b.
+
+# Let's do some calculations on very large integers.
+
+# Task
+# Read four numbers, a, b, c, and d, and print the result of a^b + c^d.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+res = pow(a, b)+pow(c, d)
+print(res)
